4086939e9bad523c0a9abddff33907bd4316078f
KVaishnavi123 <kvaishnavi135@gmail.com>
2024-07-27 12:29:16 +0530

    folder structure updated

--- template.py
+++ template.py
@@ -37,4 +37,4 @@ for filepath in list_of_files:
         os.makedirs(filedir, exist_ok=True)
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
-            pass
+            pass4

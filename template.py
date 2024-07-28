a0c7c73b5128845d282195aa911191044352b58b
KVaishnavi123 <kvaishnavi135@gmail.com>
2024-07-28 08:28:56 +0530

    folder structure updated

--- template.py
+++ template.py
@@ -5,6 +5,7 @@ package_name = "MongoDB_Connect"
 
 list_of_files = [
    ".github/workflows/ci.yaml",
+   ".github/workflows/python-publish.yaml",
    "src/__init__.py",
    f"src/{package_name}/__init__.py", 
    f"src/{package_name}/mongo_crud.py", 
